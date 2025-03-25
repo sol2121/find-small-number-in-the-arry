1c14e44a5254cbdc987793e05a8376cfaf9295cf
Balew83 <balewmolla709@gmail.com>
2025-03-26 02:01:24 +0300

    smallest number

---
+++ balew.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int findsmallest(int arr[], int size) {
+    int minNumber = arr[0]; 
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < minNumber) {
+            minNumber = arr[i]; 
+        }
+    }
+    return minNumber;
+}
+int main() {
+    int arr[] = {120, 45, 67, 89, 34, 23, 90, 11}; 
+    int size = sizeof(arr) / sizeof(arr[0]); // Calculate array size
+
+    int smallest = findsmallest(arr, size);
+    cout << "The smallest number in the array is: " << smallest << endl;
+
+    return 0;
+}
